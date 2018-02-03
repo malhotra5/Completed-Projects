5c4ccd28d49c151514f7d891cf7cc36babfef1bb
Rohit Malhotra <30446202+malhotra5@users.noreply.github.com>
2018-02-02 21:31:49 -0800

    Update Multiples of 3 and 5.py

--- Algorithims/Python/Project euler solutions/Multiples of 3 and 5.py
+++ Algorithims/Python/Project euler solutions/Multiples of 3 and 5.py
@@ -1,3 +1,10 @@
+##Problem number 1
+
+#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+#Find the sum of all the multiples of 3 or 5 below 1000.
+
+#Solution - 233168
+
 def findMultSum(rang):
     #Making a list for multiples of 3 and 5
     nums = []
@@ -17,5 +24,8 @@ def findMultSum(rang):
         sumed = sumed + i
     #Return sum
     return sumed
+
+#Printing solution
+print(findMultSum)
 
 
